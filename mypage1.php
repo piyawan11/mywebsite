57889fbce0a56d873072bfddc80dff10a4314a0e
piyawan11 <98690209+piyawan11@users.noreply.github.com>
2022-03-02 13:01:27 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -1,9 +1,9 @@
 <html>
   <head>
-    <title>
+    <title>เว็บไซต์ของปิยวรรณ</title>
   </head>
   <body>
     <a href="https://facebook.com"><h1>Facebook</h1></a>
     <a href="mypage2.php"><h1>Page 2</h1></a>
     </body>
-    </html>
+ </html>
