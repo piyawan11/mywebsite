114654ddaf302feea0a2f09a26b12a0a22b228f3
piyawan11 <98690209+piyawan11@users.noreply.github.com>
2022-03-02 12:44:28 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,7 +1,9 @@
-<html>
-  <head>
+<head>
     <title>page1</title>
   </head>
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+    <h2>
       <p><b>ชื่อ:</b> <i>นางสาวปิยวรรณ เติมเสริม</i></p>
       <p><b>ชื่อเล่น:</b> <i>ใบเตย</i></p>
       <p><b>ชั้น:</b> <i>ม.4/12</i></p>
