1b5082248c9724caf5c7acff47271dfc4fb1ff9d
piyawan11 <98690209+piyawan11@users.noreply.github.com>
2022-03-05 22:57:20 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การคำนวณรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "Myfont1";
+        src: url("Chocolateteehaypai.ttf");
+      }
+      @font-face{
+        font-family: "Myfont2";
+        src: url("MNN-Tuaklom.ttf");
+      }
+      #font1{
+        font-family: "Myfont1":
+      }
+      #font2{
+        font-family: "Myfont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาวปิยวรรณ เติมเสริม ม.4/12 เลขที่ 13 เลขประจำตัว 57034</h1>
+    <h1 id="font2">นางสาวปิยวรรณ เติมเสริม ม.4/12 เลขที่ 13 เลขประจำตัว 57034</h1>
+  </body>
+</html>
