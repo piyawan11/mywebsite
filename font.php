75d658c44cf72c6101de2fd78da2a3bde52e0746
piyawan11 <98690209+piyawan11@users.noreply.github.com>
2022-03-05 22:46:36 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,1 +1,25 @@
-
+<html>
+  <head>
+    <title>การคำนวณรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "Myfont1";
+        src: url("Fahkwang-Bold.ttf");
+      }
+      @font-face{
+        font-family: "Myfont2";
+        src: url("Fahkwang-Italic.ttf");
+      }
+      #font1{
+        font-family: "Myfont1":
+      }
+      #font2{
+        font-family: "Myfont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาวปิยวรรณ เติมเสริม ม.4/12 เลขที่ 13 เลขประจำตัว 57034</h1>
+    <h1 id="font2">นางสาวปิยวรรณ เติมเสริม ม.4/12 เลขที่ 13 เลขประจำตัว 57034</h1>
+  </body>
+</html>
