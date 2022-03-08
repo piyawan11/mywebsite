392bb65cafae992818fcc19b6cb003c6d5fd971d
piyawan11 <98690209+piyawan11@users.noreply.github.com>
2022-03-08 21:40:31 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>FB/twitter</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Luca kaneshiro</td
+        <td><img src="https://cdn.shopify.com/s/files/1/0577/1254/1891/collections/03r8xo4FygM1egk8_x400.jpg?v=1639661046"</td>
+        <td><a href="https://twitter.com/luca_kaneshiro?s=20&t=CptzxaKYXkAbyL-r3PmiPw">Twitter</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Vox Akuma</td>
+        <td><img src="https://cdn.shopify.com/s/files/1/0577/1254/1891/collections/7xOi8KmV11yciu2I_x400.jpg?v=1639661098"</td>
+        <td><a href="https://twitter.com/Vox_Akuma?s=20&t=CptzxaKYXkAbyL-r3PmiPw">Twitter</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Ike Eveland</td>
+        <td><img src="https://pin.it/6QAs7lw"</td>
+        <td><a href="https://twitter.com/ike_eveland?s=20&t=CptzxaKYXkAbyL-r3PmiPw">Twitter</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td><Shu yamino</td>
+        <td><img src="https://cdn.shopify.com/s/files/1/0577/1254/1891/collections/7QaPU8DF03UA9Flw_x400.jpg?v=1639661033"</td>
+        <td><a href="https://twitter.com/shu_yamino?s=20&t=CptzxaKYXkAbyL-r3PmiPw">Twitter</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td><Mysta rias</td>
+        <td><img src="https://cdn.shopify.com/s/files/1/0577/1254/1891/collections/CeMBhwhqCvsOp4I5_x400.jpg?v=1639661113"</td>
+        <td><a href="https://twitter.com/Mysta_Rias?s=20&t=CptzxaKYXkAbyL-r3PmiPw">Twitter</td>
+      </tr>
+    </table>
+  </body>
+</html> 
